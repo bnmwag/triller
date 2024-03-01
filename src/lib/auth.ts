56ed3bbb504ca4ff729052f5e706bea3b0938273
bnmwag <bnm.wag@gmail.com>
2024-03-01 14:29:20 +0100

    feat: handling auth
    - update next-auth version
    - GH OAuth
    - drizzle adapter
    - neon db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,9 @@
-import { NextAuthOptions } from "next-auth";
-import GithubProvider, { GithubProfile } from "next-auth/providers/github";
+import { db } from "@/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/GitHub";
 
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_URL,
-  session: { strategy: "jwt" },
-  pages: {
-    signIn: "/sign-in",
-    signOut: "/sign-out",
-    error: "/error",
-  },
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    session: ({ session, token }) => {
-      return { ...session, id: token.id, profile: token.profile };
-    },
-    jwt: async ({ token, user, profile }) => {
-      if (user) return { ...token, id: user.id, profile };
-
-      return token;
-    },
-  },
-};
+export const { handlers, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [GitHub],
+});
