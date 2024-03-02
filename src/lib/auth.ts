4205175ecf57939995437525d45af5614ba7e051
bnmwag <bnm.wag@gmail.com>
2024-03-02 16:58:37 +0100

    fix: "module not found" build error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/GitHub";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
